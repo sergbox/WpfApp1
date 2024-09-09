5483eaf339eb187d3d103c112244b680ab1bb699
amax <mr.amax2000@mail.ru>
2024-09-09 12:17:00 +0300

    1

---
+++ UserControlWeather.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.DataModel;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlWeather.xaml
+    /// </summary>
+    public partial class UserControlWeather : UserControl
+    {
+        public UserControlWeather()
+        {
+            InitializeComponent();
+        }
+        public UserControlWeather(Weather weather)
+        {
+            InitializeComponent();
+            System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["weatherViewSource"];
+            myCollectionViewSource.Source = new List<Weather>() { weather};
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
